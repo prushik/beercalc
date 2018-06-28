31996ba9048bff440f034e338dbadf7197bdbe67
Philip Rushik <prushik@gmail.com>
2018-06-27 20:37:22 -0400

    fix caps and add ibu formulas

--- README.md
+++
@@ -1,3 +0,0 @@
-# beercalc
-
-A beer recipe calculator, for command line use or serving through the unweave webserver
---
+++ src/ibu.c
@@ -0,0 +1,6 @@
+// rager method
+// ibu = (Woz * U * a * 7489) / (Vgal * Cgravity)
+
+// tinseth method
+// ibu = (1.65 * 0.000125^(Ggravity - 1)) * ((1 - e^(-0.04*time)) * a * m * 1000
+// ibu = (1.65*0.000125^(gravity-1))*(1-EXP(-0.04*time))*alpha*mass*1000
