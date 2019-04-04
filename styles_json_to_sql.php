51a3e8cd9cc6bfdab8b8c6ee64dad1d4dd1e4562
Philip Rushik <prushik@gmail.com>
2019-04-04 10:21:43 -0400

    add brewers association styles

--- styles_json_to_sql.php
+++ styles_json_to_sql.php
@@ -1,20 +1,20 @@
 #!/usr/bin/php
 <?php
 
-$styles = json_decode(file_get_contents('styles_fmt.json'));
+$styles = json_decode(file_get_contents('ba2018_styles.json'));
 
 $index = 0;
 
-foreach ($styles as $category => $substyles)
+foreach ($styles->styles as $category => $substyles)
 {
-	foreach ($substyles as $name => $style)
+	foreach ($substyles->styles as $style)
 	{
-		echo "insert into style values (\"" . $name . "\"," .
-			$style->gu[0] . "," . $style->gu[1] . "," .
+		echo "insert into style values (\"" . $style->name . "\"," .
+			$style->og[0] . "," . $style->og[1] . "," .
 			$style->fg[0] . "," . $style->fg[1] . "," .
 			$style->ibu[0] . "," . $style->ibu[1] . "," .
 			$style->srm[0] . "," . $style->srm[1] . "," .
-			$style->carb[0] . "," . $style->carb[1] . "," .
+			0 . "," . 0 . "," .
 			$style->abv[0] . "," . $style->abv[1] . "," .
 			$index++ .
 			");\n";
